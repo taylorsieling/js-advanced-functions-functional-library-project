2f4113a2d7f3e2e8266bd9e6415bede64b790787
taylorsieling <taylorasieling@gmail.com>
2021-03-24 16:51:48 -0500

    commit tree 0020ef7a24781f7cb275cc9dcef0c609cb27a6f0

--- index.js
+++ index.js
@@ -4,11 +4,23 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, callback) {
+      // iterate over collection
+      // if collection is an object, callback's arguements will be value, key, collection
+      // return orignal collection
+      let array = []
+      if (typeof collection === 'object') {
+        array = (Object.values(collection))
+      } else {
+        array = collection
+      }
+      array.forEach(el => {
+        return callback(el)
+      })
+      return collection
     },
 
-    map: function() {
+    map: function(collection, callback) {
 
     },
 
